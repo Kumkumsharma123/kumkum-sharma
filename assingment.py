ac44fad24ea5a1c2593dc45d1b63b4925f0e8183
Kumkumsharma123 <113447608+Kumkumsharma123@users.noreply.github.com>
2022-09-14 18:41:44 +0530

    Create assingment.py

---
+++ assingment.py
@@ -0,0 +1,10 @@
+Dict =
+{
+"Username": ("Admin", "Admin1"), "Password": "Pass", "Os": "windows", "Ram":"128GB",}
+
+print (Dict["Username"])
+#update
+  Dict["model"] = 32667
+print (Dict)
+     del Dict["Os"] print (Dict)
+
